6b39f8c529cb0673b1da7926bcdf31003b945f4e
majid <majid@LAPTOP-I6E7CE5T>
2023-03-21 18:59:49 +0100

    Implementation using lowerBound and upperBound

--- intervals/src/intervals/Interval.java
+++ intervals/src/intervals/Interval.java
@@ -1,6 +1,6 @@
 package intervals;
 
-/*
+/**
  * procedure for defining a data abstraction:
  * 1. Define the raw abstraction state space
  * 		= declare the inspectors (specify name and return type, but not yet the body
@@ -10,6 +10,18 @@ package intervals;
  * 
  * 
  */
+
+/**procedure for implementing a data abstraction:
+ * 1. Define the raw concrete state space =
+ * 	  define the fields
+ * 2. Define the valid concrete state space=
+ *    write the representation invariance (constraints on the values of the fields, we write in a java doc comment above the fields
+ * 3. Define the mapping from concrete states to abstract states=
+ *    implement the inspectors (getters)
+ *    Note: every valid concrete state must map to a valid abstract state, so the corresponding abstract state must satisfy the abstract state invariance otherwise there is something wrong with your class
+ * 4. Implement the factory methods and the mutator methods
+ */
+
 
 /**
  * Abstract  state invariants: (you don't need to necessarily write this)
@@ -22,19 +34,30 @@ package intervals;
  */
 
 public class Interval {
+	//We want to make sure client can't access the method that's why we use private, if we want to change these fields, client won't break
+	
+	/**
+	 * Representation invariants:
+	 * 
+	 * @invar | lowerBound <= upperBound (every pair of numbers where the first is not greater than the second one defines a valid concrete state for the interval class)
+	 */
+	
+	private int lowerBound;
+	private int upperBound;
 
-	public static int getLowerBound(Interval interval) {throw new RuntimeException ("not yet implemented"); } // we can implement methods before writing them out
+	public static int getLowerBound(Interval interval) {return interval.lowerBound; } // we can implement methods before writing them out, we then write: throw new RuntimeException ("not yet implemented");
 	
-	public static int getUpperBound(Interval interval) {throw new RuntimeException ("not yet implemented"); }
+	public static int getUpperBound(Interval interval) {return interval.upperBound; }
 	
-	public static int getLength (Interval interval) {throw new RuntimeException ("not yet implemented"); }
+	public static int getLength (Interval interval) {return(interval.upperBound-interval.lowerBound); }
 	
 	// you can call this method a derived inspector
 		/**
 		 *@post | result == (getLowerBound(interval) <= x && x < getUpperBound(interval))
 		 * 
 		 */
-	public static boolean cantains (Interval interval, int x){throw new RuntimeException ("not yet implemented"); }
+	public static boolean cantains (Interval interval, int x){
+		return getLowerBound(interval) <= x && x < getUpperBound(interval); }
 	
 	
 	//allow way for client to create interval objects:
@@ -44,7 +67,12 @@ public class Interval {
 	 * @post | getUpperBound (result) == upperBound
 	 */
 	
-	public static Interval create(int LowerBound, int UpperBound) {throw new RuntimeException ("not yet implemented"); }
+	public static Interval create(int lowerBound, int upperBound) {
+		Interval result = new Interval();
+		result.lowerBound= lowerBound;
+		result.upperBound=upperBound;
+		return result;
+		}
 	/**
 	 * @pre | interval !=null
 	 * @pre | other !=null
@@ -53,7 +81,8 @@ public class Interval {
 	 * @post | getLength(
 	 */
 	
-	public static Interval plus (Interval interval, Interval other) {throw new RuntimeException ("not yet implemented"); }
+	public static Interval plus (Interval interval, Interval other) {
+		return create(interval.lowerBound+other.lowerBound, interval.upperBound + other.upperBound); }
 	
 	// We have now defined an API for the data abstraction
 	
